fd983c39b9afe1c2f659c97a4970b9f58b30a432
James Gary <gmarchesi888@gmail.com>
2019-09-04 16:57:24 -0400

    Connecting to Express server.

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -1,12 +1,77 @@
 import React, { Component } from "react";
 
+class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
 
-class CreateTodo extends Component{
-    render(){
-        return(
-            <h2>I Am Create Todo</h2>
-        )
-    }
+    this.initialState = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+
+    this.state = this.initialState;
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    console.log(this.state);
+    this.setState({ [name]: value });
+  };
+
+  submitForm = () => {
+    // this.props.handleSubmit(this.state);
+    console.log("Data submitted", this.state);
+    this.setState(this.initialState);
+  };
+
+  render() {
+    const {
+      todo_description,
+      todo_responsible,
+      todo_priority,
+      todo_completed
+    } = this.state;
+
+    return (
+      <div className="container-inner">
+        <h2>Create Todo</h2>
+        <form className="h-form" onSubmit={this.onSubmit}>
+          <label className="h-label">Description</label>
+          <input
+            className="h-input"
+            type="text"
+            name="todo_description"
+            value={todo_description}
+            onChange={this.handleChange}
+          />
+          <label className="h-label">Responsible</label>
+          <input
+            className="h-input"
+            type="text"
+            name="todo_responsible"
+            value={todo_responsible}
+            onChange={this.handleChange}
+          />
+          <label className="h-label">Priority</label>
+          <input
+            className="h-input"
+            type="text"
+            name="todo_priority"
+            value={todo_priority}
+            onChange={this.handleChange}
+          />
+          <input
+            type="button"
+            className="h-btn"
+            value="Submit"
+            onClick={this.submitForm}
+          />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default CreateTodo;
